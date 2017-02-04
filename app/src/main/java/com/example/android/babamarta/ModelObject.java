a146d296c893fb33e95ac9af6de567bb91a77ae2
Kosta Dinkov <k.v.dinkov@gmail.com>
2017-02-04 10:48:50 +0200

    Working Version 2 : -removed .xml code repetition -added custom text size dimensions for larger devices (tablet) -the resources in the page layout are being updated dynamically

--- app/src/main/java/com/example/android/babamarta/ModelObject.java
+++ app/src/main/java/com/example/android/babamarta/ModelObject.java
@@ -1,17 +1,29 @@
 package com.example.android.babamarta;
 
 public enum ModelObject {
-    FIRST(R.layout.view_first),
-    SECOND(R.layout.view_second),
-    THIRD(R.layout.view_third);
+    FIRST(R.layout.page_layout, R.drawable.bm1, R.string.first_info),
+    SECOND(R.layout.page_layout, R.drawable.bm5, R.string.second_info),
+    THIRD(R.layout.page_layout, R.drawable.bm7, R.string.third_info);
 
     private int mLayoutResourceId;
+    private int mImgId;
+    private int mInfoId;
 
-    ModelObject(int layoutResourceId) {
+    ModelObject(int layoutResourceId, int imgId, int infoId) {
         mLayoutResourceId = layoutResourceId;
+        mImgId = imgId;
+        mInfoId = infoId;
     }
 
     public int getLayoutResourceId() {
         return mLayoutResourceId;
+    }
+
+    public int getImageId() {
+        return mImgId;
+    }
+
+    public int getmInfoId() {
+        return mInfoId;
     }
 }
--- app/src/main/java/com/example/android/babamarta/ViewPagerAdapter.java
+++ app/src/main/java/com/example/android/babamarta/ViewPagerAdapter.java
@@ -5,6 +5,8 @@ import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class ViewPagerAdapter extends PagerAdapter {
@@ -21,6 +23,12 @@ public class ViewPagerAdapter extends PagerAdapter {
         LayoutInflater inflater = LayoutInflater.from(mContext);
 
         ViewGroup layout = (ViewGroup) inflater.inflate(modelObject.getLayoutResourceId(), collection, false);
+
+        ImageView imageView = (ImageView) layout.findViewById(R.id.imageBackground);
+        TextView textView = (TextView) layout.findViewById(R.id.info_text);
+
+        imageView.setImageResource(modelObject.getImageId());
+        textView.setText(modelObject.getmInfoId());
 
         collection.addView(layout);
         return layout;
